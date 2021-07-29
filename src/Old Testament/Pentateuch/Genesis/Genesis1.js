26c3e2849e2921963033083c8efff1d88f2c3463
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-07-29 14:01:51 -0400

    Added routing for the book of Genesis as well as files for all 50 chapters

---
+++ src/Old Testament/Pentateuch/Genesis/Genesis1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Genesis1() {
+    return (
+
+        <>
+            <h2>Genesis 1</h2>
+        </>
+    )
+}
+
+export default Genesis1;
---
+++ src/Old Testament/Pentateuch/Genesis/genesis2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Genesis2() {
+    return (
+
+        <>
+            <h2>Genesis 2</h2>
+        </>
+    )
+}
+
+export default Genesis2;
